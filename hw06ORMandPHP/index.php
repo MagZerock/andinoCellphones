ab1af03e281fc17cb602aebbbbdeec7f85b8e68c
Andino Martínez Nelson David <ndandino@espe.edu.ec>
2026-05-11 14:48:17 -0500

    exam

--- hw06ORMandPHP/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('Location: src/view/index.html');
-exit;
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
 
-require_once __DIR__ . '/app/Vista/menu.php';
+require_once __DIR__ . '/app/Vista/index.html';
 ?>
